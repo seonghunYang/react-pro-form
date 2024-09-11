4ff545f40abd92db3259721d4758183350e1bee6
SeonghunYang <didtjdgns852@naver.com>
2024-09-11 04:33:44 +0000

    정렬한 결과를 화면에 보이도록 하는 함수 작성

--- src/components/progressive-form.tsx
+++ src/components/progressive-form.tsx
@@ -111,15 +111,35 @@ function topologicalSort(
   return result;
 }
 
+function getSteps(children: React.ReactNode, stepKeys: string[]) {
+  const result: React.ReactNode[] = [];
+  stepKeys.forEach((stepKey) => {
+    React.Children.toArray(children).forEach((child) => {
+      if (!React.isValidElement(child)) return;
+      if (child.type !== Step) return;
+      if ((child.props as Step).stepKey === stepKey) {
+        result.push(child);
+      }
+    });
+  });
+  return result;
+}
+
 function ProgressiveFormRoot({ children }: React.PropsWithChildren<{}>) {
-  // children의 deponds에 값들이 tocued 되었는지에 따라서 보여줅지 말지 확인
   const ref = useRef<HTMLFormElement>(null);
   const [formState, setFormState] = useState<Record<string, any>>({});
+  const [eventLogger, setEventLogger] = useState<string[]>([]);
+
+  const sortedStepKey = topologicalSort(
+    formState,
+    ...getDependencyGraph(children)
+  );
 
   // 노드가 바뀔 수 있으니 이벤트 재등록
   // 대신 formstate이 값이 바뀔 때 마다 갱신되지 않음
   useEffect(() => {
-    const handleChage = (element: FormElements) => {
+    const handleChange = (event: Event) => {
+      const element = event.target as FormElements;
       setFormState((prev) => ({ ...prev, [element.name]: element.value }));
     };
 
@@ -127,9 +147,7 @@ function ProgressiveFormRoot({ children }: React.PropsWithChildren<{}>) {
       for (let i = 0; i < ref.current.elements.length; i++) {
         const element = ref.current.elements[i] as FormElements;
 
-        element.addEventListener("change", (event) => {
-          handleChage(event.target as FormElements);
-        });
+        element.addEventListener("change", handleChange);
       }
     }
 
@@ -137,44 +155,26 @@ function ProgressiveFormRoot({ children }: React.PropsWithChildren<{}>) {
       if (ref.current) {
         for (let i = 0; i < ref.current.elements.length; i++) {
           const element = ref.current.elements[i] as FormElements;
-          element.removeEventListener("change", (event) => {
-            handleChage(event.target as FormElements);
-          });
+          element.removeEventListener("change", handleChange);
         }
       }
     };
   }, [formState]);
 
   return (
-    <form ref={ref}>
-      {React.Children.toArray(children).filter((child) => {
-        if (!React.isValidElement(child)) return false;
-        if (child.type !== Step) return false;
-
-        const { depends } = child.props as Step;
-        // depends 의 값이 특정 조건을 만족하는지 확인
-        if (depends === undefined) return true;
-
-        if (Array.isArray(depends)) {
-          return depends.every((depend) => {
-            if (formState[depend]) return true;
-            return false;
-          });
+    <form
+      ref={ref}
+      onSubmit={(event) => {
+        event.preventDefault();
+      }}
+      onKeyUp={(event) => {
+        if (event.key === "Enter") {
+          event.preventDefault();
+          console.log(event);
         }
-
-        if (typeof depends === "string") {
-          if (formState[depends]) return true;
-          return false;
-        }
-
-        if (typeof depends === "object") {
-          return Object.keys(depends).every((key) => {
-            if (formState[key] === depends[key]) return true;
-            return false;
-          });
-        }
-        return false;
-      })}
+      }}
+    >
+      {getSteps(children, sortedStepKey)}
     </form>
   );
 }
@@ -193,7 +193,6 @@ function Step({
 }: React.PropsWithChildren<Step>) {
   return <>{children}</>;
 }
-
 
 const findInputName = (children: React.ReactNode) => {
   const result: string[] = [];
